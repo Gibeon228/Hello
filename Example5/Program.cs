a2eded360c6d09bdc53b17a26620dffac9ac2a9c
Vladimir <VL97@mail.ru>
2022-06-04 15:41:16 +0300

    Условие вывода про Машу

---
+++ Example5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя ");
+String username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Это Маша");
+}
+else 
+{
+    Console.Write("Привет ");
+    Console.WriteLine(username);
+}
